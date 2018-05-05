361d022e201be187b5e9e9365c7d0037159fb948
soasme <soasme@gmail.com>
2018-05-05 23:37:49 +1200

    load fonts.

---
+++ src/assets/init.lua
@@ -0,0 +1,8 @@
+local Assets = Class 'Assets'
+
+Assets.font_s = love.graphics.newFont('assets/font.ttf', 8)
+Assets.font_m = love.graphics.newFont('assets/font.ttf', 14)
+Assets.font_l = love.graphics.newFont('assets/font.ttf', 28)
+Assets.font_xl = love.graphics.newFont('assets/font.ttf', 56)
+
+return Assets
