39270b7da16d59ec7db77dd818eb088fe5273da3
soasme <soasme@gmail.com>
2018-05-06 22:20:47 +1200

    rename to play state.

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -1,21 +1,23 @@
-local MenuState = Class 'MenuState'
+local PlayState = Class 'PlayState'
 
-function MenuState:enter()
+function PlayState:enter()
     _G.player = require('entities.Player')(
             love.graphics.getWidth() * 1 / 4,
             love.graphics.getHeight() * 2 / 3)
     _G.world = Tiny.world(
+        require('systems.EphemeralSystem')(),
         require('systems.GameControlSystem')(),
         require('systems.UpdateSystem')(),
         require('systems.CollisionSystem')(Bump.newWorld(32)),
         require('systems.SpriteSystem')(),
+        require('entities.WormHole')(),
         _G.player
     )
 
 end
 
-function MenuState:draw()
+function PlayState:draw()
     world:update(love.timer.getDelta())
 end
 
-return MenuState
+return PlayState
